70b77453a9e63940e9fcce4d2ea12ea5a9a5d5e9
Pavel Nakonechny <pavel.nakonechny@skitlab.ru>
2013-02-26 14:52:49 +0400

    Helper scripts for automated build of FC were added

---
+++ build.sh
@@ -0,0 +1,24 @@
+. $HOME/j.skit/dm8168/ti-env.sh
+
+make \
+ XDAIS_INSTALL_DIR=$THIRD_PARTY_DIR/xdais \
+ XDC_INSTALL_DIR=$THIRD_PARTY_DIR/xdctools \
+ BIOS_INSTALL_DIR=$THIRD_PARTY_DIR/bios \
+ CMEM_INSTALL_DIR=$THIRD_PARTY_DIR/linuxutils \
+ OSAL_INSTALL_DIR=$THIRD_PARTY_DIR/osal \
+ EDMA3_LLD_INSTALL_DIR=$THIRD_PARTY_DIR/edma3lld \
+ IPC_INSTALL_DIR=$THIRD_PARTY_DIR/ipc \
+ LINK_INSTALL_DIR=$THIRD_PARTY_DIR/syslink \
+ DESTDIR=$THIRD_PARTY_DIR/framework_components/tmp \
+ CGTOOLS_V5T=$CROSSCOMPILER_DIR/../ \
+ CC_V5T=bin/${CROSS_COMPILE}gcc \
+ -f framework_components.mak \
+all
+
+# ti.targets.C64P= \
+# ti.targets.C674= \
+# ti.targets.elf.C64P= \
+# ti.targets.elf.C64T= \
+# ti.targets.elf.C66= \
+# ti.targets.elf.C674= \
+# ti.targets.arm.elf.M3= \
---
+++ clean.sh
@@ -0,0 +1,6 @@
+. $HOME/j.skit/dm8168/ti-env.sh
+
+make \
+ XDC_INSTALL_DIR=$THIRD_PARTY_DIR/xdctools \
+ -f framework_components.mak \
+clean
